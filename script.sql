7e28579c8133dff3b9202a03339f8b73b7d6c3c4
Luís Campanholi <lpcampanholi@gmail.com>
2024-05-01 17:59:42 -0300

    correção dos nomes

--- script.sql
+++ script.sql
@@ -1,28 +1,54 @@
 -- Trazer os dados da tabela fornecedores
-SELECT * FROM tabelafornecedores;
+SELECT * FROM Fornecedores;
 
-SELECT * FROM tabelafornecedores WHERE país_de_origem = 'China';
+SELECT * FROM Fornecedores WHERE pais_de_origem = 'China';
 
 -- Busca dados únicos, elimina duplicatas
-SELECT DISTINCT cliente FROM tabelapedidos;
-
-CREATE TABLE tabelaclientes (
-    ID_Cliente INT PRIMARY KEY,
-    Nome_Cliente VARCHAR (250),
-    Informacoes_de_Contato VARCHAR (250)
-);
+SELECT DISTINCT cliente FROM Pedidos;
 
 CREATE DATABASE BibliotecaDB;
 
 CREATE SCHEMA LivroSchema;
 
 -- Alterar Tabela, adicionar Coluna
-ALTER TABLE tabelaclientes
-ADD Endereco_cliente VARCHAR (250);
+ALTER TABLE Clientes
+ADD endereco_cliente VARCHAR (250);
 
 -- Apaga Tabela
-DROP TABLE tabelaclientes;
+DROP TABLE Clientes;
 
 -- Alterar Tabela, apagar Coluna
-ALTER TABLE estudantes
-DROP COLUMN Idade;
+ALTER TABLE Estudantes
+DROP COLUMN idade;
+
+CREATE TABLE Clientes (
+    id_cliente INT PRIMARY KEY,
+    nome_cliente VARCHAR (250),
+    informacoes_de_contato VARCHAR (250),
+    endereco_cliente VARCHAR (250)
+);
+
+CREATE TABLE Categorias (
+  id_categoria INT PRIMARY KEY,
+  nome_categoria VARCHAR (250),
+  descricao_categoria TEXT
+);
+
+-- Criar tabela com chaves estrangeiras
+CREATE TABLE Produtos (
+  id_produto INT PRIMARY KEY,
+  nome_produto VARCHAR (250),
+  descricao TEXT,
+  categoria INT,
+  preco_de_compra DECIMAL (10,2),
+  unidade VARCHAR (50),
+  fornecedor INT,
+  data_de_inclusao DATE,
+  FOREIGN KEY (Categoria) REFERENCES categorias (id_categoria),
+  FOREIGN KEY (Fornecedor) REFERENCES fornecedores (id_fornecedor)
+);
+
+-- Adicionar coluna com chave estrangeira a uma tabela
+ALTER TABLE Produtos
+ADD COLUMN fk_fornecedor INTEGER 
+REFERENCES Fornecedores(id_fornecedor);

63574a2c267cccf2627250bf2160202f035dc37a
Luís Campanholi <lpcampanholi@gmail.com>
2024-05-14 21:34:22 -0300

    operadores

--- Curso02/script.sql
+++ Curso02/script.sql
@@ -7,7 +7,46 @@ LIMIT 5;
 SELECT * FROM Colaboradores
 LIMIT 10 OFFSET 10;
 
+-- IS NULL / NOT NULL
 SELECT * FROM HistoricoEmprego
-WHERE DataTermino ISNULL
+WHERE DataTermino IS NULL
 ORDER BY salario DESC
 LIMIT 5;
+
+-- %
+SELECT * FROM Treinamento
+WHERE Curso LIKE 'O poder%';
+
+SELECT * FROM Treinamento
+WHERE Curso LIKE '%realizar%';
+
+SELECT * FROM Colaboradores
+WHERE Nome LIKE 'Isadora%';
+
+-- AND
+SELECT * FROM HistoricoEmprego
+WHERE Cargo = 'Professor' AND
+datatermino NOT NULL;
+
+-- OR
+SELECT * FROM HistoricoEmprego
+WHERE Cargo = 'Oftalmologista' OR
+Cargo = 'Dermatologista';
+
+-- IN / NOT IN
+SELECT * FROM HistoricoEmprego
+WHERE Cargo IN ('Oftalmologista', 'Dermatologista', 'Professor');
+
+SELECT * FROM HistoricoEmprego
+WHERE Cargo NOT IN ('Oftalmologista', 'Dermatologista', 'Professor');
+
+SELECT * FROM Treinamento
+WHERE (Curso LIKE '%O direito%' and Instituicao = 'da Rocha')
+OR (Curso LIKE 'O conforto%' AND Instituicao = 'das Neves');
+
+SELECT * FROM livros
+WHERE (genero LIKE '%ficção científica%' OR genero LIKE '%fantasia%')
+AND ano_publicacao > 2000
+AND editora IS NULL
+ORDER BY ano_publicacao DESC
+LIMIT 20;

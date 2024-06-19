1f6442600e2f2fd0b502273fbdca79bbfe5218db
Luís Campanholi <lpcampanholi@gmail.com>
2024-06-19 07:40:57 -0300

    Views

--- Curso03/script.sql
+++ Curso03/script.sql
@@ -30,7 +30,7 @@ FROM produtos
 GROUP BY nome, preco
 HAVING preco > (SELECT AVG(preco) FROM produtos);
 
--- JOINS
+-- INNER JOIN
 
 SELECT c.nome, p.id, p.datahorapedido
 FROM clientes c
@@ -41,6 +41,8 @@ SELECT p.id, c.nome, p.datahorapedido, p.status
 FROM pedidos p
 LEFT JOIN clientes c
 ON p.idcliente = c.id;
+
+-- RIGHT JOIN
 
 SELECT pr.nome, ip.idproduto, ip.idpedido
 FROM itensdepedidos ip
@@ -65,5 +67,85 @@ LEFT JOIN pedidos p
 ON c.id = p.IDcliente
 WHERE p.IDcliente IS NULL;
 
--- leftb join
+-- LEFT JOIN
 
+SELECT c.nome, p.id
+FROM clientes c
+LEFT JOIN pedidos p
+ON c.id = p.idcliente
+WHERE p.id IS NULL;
+
+SELECT c.nome, x.id
+FROM clientes c
+LEFT JOIN
+(
+  SELECT p.id, p.idcliente
+  FROM pedidos p
+  WHERE strftime('%m', p.DataHoraPedido) = '09') x
+ON c.id = x.idcliente
+WHERE x.idcliente IS NULL;
+
+-- FULL JOIN
+SELECT c.nome, p.id
+FROM clientes c
+FULL JOIN pedidos p
+ON c.id = p.idcliente
+WHERE p.id IS NULL;
+
+-- ---
+
+SELECT p.id, pr.nome, ip.quantidade, pr.preco, ip.precounitario
+from pedidos p
+JOIN itensdepedidos ip ON p.id = ip.idpedido
+JOIN produtos pr ON pr.id = ip.idproduto;
+
+-- Total do faturamento por pedido
+
+SELECT p.id, c.nome, p.datahorapedido, SUM(ip.precounitario) AS ValorTotalPedido
+FROM clientes c
+JOIN pedidos p ON c.id = p.idcliente
+JOIN itensdepedidos ip ON p.id = ip.idpedido
+GROUP BY p.id, c.nome;
+
+-- Total do faturamento de cada cliente
+
+SELECT c.nome, count(c.nome) QuantidadeDePedidos, SUM(ip.precounitario) SomaPorCliente
+FROM clientes c
+JOIN pedidos p ON c.id = p.idcliente
+JOIN itensdepedidos ip ON p.id = ip.idpedido
+GROUP BY c.nome
+ORDER BY SomaPorCliente DESC;
+
+-- VIEWS
+
+CREATE VIEW ViewTelefonesClientes AS
+SELECT nome, telefone FROM clientes;
+
+CREATE VIEW ViewValorTotalPedido AS
+SELECT p.id, c.nome, p.datahorapedido, SUM(ip.precounitario) AS ValorTotalPedido
+FROM clientes c
+JOIN pedidos p ON c.id = p.idcliente
+JOIN itensdepedidos ip ON p.id = ip.idpedido
+GROUP BY p.id, c.nome;
+
+CREATE VIEW ViewValorTotalPorCliente AS
+SELECT c.nome, count(c.nome) QuantidadeDePedidos, SUM(ip.precounitario) SomaPorCliente
+FROM clientes c
+JOIN pedidos p ON c.id = p.idcliente
+JOIN itensdepedidos ip ON p.id = ip.idpedido
+GROUP BY c.nome
+ORDER BY SomaPorCliente DESC;
+
+SELECT * FROM ViewTelefonesClientes;
+
+SELECT * FROM ViewValorTotalPedido;
+
+SELECT * FROM ViewValorTotalPorCliente;
+
+SELECT * FROM ViewValorTotalPedido
+where valortotalpedido > 10 AND valortotalpedido < 14;
+
+SELECT * FROM ViewValorTotalPedido
+WHERE strftime('%m', datahorapedido) = '08';
+
+DROP VIEW ViewTelefonesClientes;

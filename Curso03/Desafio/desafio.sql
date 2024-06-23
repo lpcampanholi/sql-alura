8d068443a50eb0501814e888b829d2e5bd6d3828
Luís Campanholi <lpcampanholi@gmail.com>
2024-06-23 09:22:52 -0300

    exercíciosss

---
+++ Curso03/Desafio/desafio.sql
@@ -0,0 +1,9 @@
+Consulta 1: Buscar o nome do professor e a turma que ele é orientador
+
+Consulta 2: Retornar o nome e a nota do aluno que possui a melhor nota na disciplina de Matemática
+
+Consulta 3: Identificar o total de alunos por turma
+
+Consulta 4: Listar os Alunos e as disciplinas em que estão matriculados
+
+Consulta 5: Criar uma view que apresenta o nome, a disciplina e a nota dos alunos

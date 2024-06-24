6edf470936be519b7020eca6fd6d60adaeb11322
Luís Campanholi <lpcampanholi@gmail.com>
2024-06-24 06:56:45 -0300

    tratamento de dados

--- Curso04/script.sql
+++ Curso04/script.sql
@@ -22,10 +22,23 @@ SELECT COUNT(*) as Qtd, 'Vendas' as Tabela FROM vendas;
 
 BEGIN TRANSACTION;
 
-UPDATE produtos SET preco = 200 WHERE nome_produto LIKE '%chocolate%' AND preco > 200;
+UPDATE produtos
+SET preco =
+  CASE
+  WHEN nome_produto = 'Bola de Futebol' AND preco < 20 THEN 20
+  WHEN nome_produto = 'Bola de Futebol' AND preco > 100 THEN 100
+  WHEN nome_produto = 'Chocolate' AND preco < 10 THEN 10
+  WHEN nome_produto = 'Chocolate' AND preco > 50 THEN 50
+  WHEN nome_produto = 'Celular' AND preco < 80 THEN 80
+  WHEN nome_produto = 'Celular' AND preco > 5000 THEN 5000
+  WHEN nome_produto = 'Livro de Ficção' AND preco < 10 THEN 10
+  WHEN nome_produto = 'Livro de Ficção' AND preco > 200 THEN 200
+  WHEN nome_produto = 'Camisa' AND preco < 80 THEN 80
+  WHEN nome_produto = 'Camisa' AND preco > 200 THEN 200
+  ELSE preco
+  END
+WHERE categoria_id IN (1, 2, 3, 4, 5);
 
-SELECT * FROM produtos
-WHERE nome_produto LIKE '%chocolate%';
+SELECT * FROM produtos;
 
 COMMIT;
-
